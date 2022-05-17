221722da85e003232ea102c8fd3d6e35ae55f157
Ruchet <ruchetkokare0495@gmail.com>
2022-05-17 11:08:02 +0530

    added pipe,directive and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { MainContainerComponent } from './main-container/main-container.component';
+import { HeighlightDirective } from './heighlight.directive';
+import { ImgularPipe } from './imgular.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    UserListComponent,
+    MainContainerComponent,
+    HeighlightDirective,
+    ImgularPipe
   ],
   imports: [
     BrowserModule
---
+++ src/app/imgular.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ImgularPipe } from './imgular.pipe';
+
+describe('ImgularPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ImgularPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
